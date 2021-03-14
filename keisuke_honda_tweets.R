9415b6e043680ad9e7a2ca2bc902c779cc3c9fa3
Dan Orlowitz <dan.orlowitz@gmail.com>
2021-03-14 20:29:46 +0900

    Initial commit

---
+++ keisuke_honda_tweets.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(rtweet)
+library(ggplot2)
+test <- search_tweets("keisuke honda", n=100,include_rts=FALSE) #pulls tweets
+languages <- test %>% #creates new df
+  select(user_id,lang) %>% #only pulls relevant columns
+  group_by(lang) %>% #groups rows by language
+  summarize(count = n()) #adds count for each language
+  vis <- ggplot(languages,aes(x = lang, y = count))+ #creates plot
+  geom_bar(aes(x = reorder(lang, -count), fill = reorder(lang, -count)), stat = "identity")+ #adds bars to plot, orders & colors by language
+  labs(title="Keisuke Honda tweets",subtitle="By language",fill="Language")+ #adds labels
+  xlab("Language of tweet")+
+  ylab("Number of tweets")+
+ggsave("keisukehonda_bylanguage.png")
